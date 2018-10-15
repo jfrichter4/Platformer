5787df89b9db082bc404ccfe9a8bdc12fa3ff593
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-15 13:01:49 -0400

    Updated from Brython Server: 10/15/2018 1:01:55 PM

--- platformer.py
+++ platformer.py
@@ -8,9 +8,9 @@ https://github.com/HHS-IntroProgramming/Platformer
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
-
+WW = 1000
+WH = 800
+#Colors Set:
 blue = Color(0x2EFEC8, 1.0)
 black = Color(0x000000, 1.0)
 pink = Color(0xFF00FF, 1.0)
@@ -22,7 +22,7 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
 grey = Color(0xC0C0C0, 1.0)
-
+#Lines:
 thinline = LineStyle(2, black)
 blkline = LineStyle(1, black)
 noline = LineStyle(0, white)
@@ -32,4 +32,57 @@ redline = LineStyle(1, red)
 greenline = LineStyle(1, green)
 gridline = LineStyle(1, grey)
 grid=RectangleAsset(30,30,gridline,white)
+windowSurface.fill(white)
+while True:
+    for event in ggame.event.get():
+        if event.type == QUIT:
+            ggame.quit()
+            sys.exit()
+#Dir:
+DL = 'downleft'
+DR = 'downright'
+UL = 'upleft'
+UR = 'upright'
+MS = 4
+
+for b in boxes:
+    # Move the character data structure
+    if b['dir'] == DL:
+        b['rect'].left -= MS
+        b['rect'].top += MS
+    if b['dir'] == DR:
+        b['rect'].left += MS
+        b['rect'].top += MS
+    if b['dir'] == UL:
+        b['rect'].left -= MS
+        b['rect'].top -= MS
+    if b['dir'] == UR:
+        b['rect'].left += MS
+        b['rect'].top -= MS
+    # Check whether the character has moved out of the window
+    if b['rect'].top < 0:
+        # The box has moved past the top
+        if b['dir'] == UL:
+            b['dir'] == DL
+        if b['dir'] == UR:
+            b['dir'] == DR
+        if b['rect'].bottom > WH:
+            # The box has moved past the bottom
+            if b['dir'] == DL:
+                b['dir'] == UL
+            if b['dir'] == DR:
+                b['dir'] == UR
+        if b['rect'].left < 0:
+            # The box has moved past the left side
+            if b['dir'] == DL:
+                b['dir'] == DR
+            if b['dir'] == UL:
+                b['dir'] == UR
+        if b['rect'].right > WW:
+            # The character has moved past the right side
+            if b['dir'] == DR:
+                b['dir'] == DL
+            if b['dir'] == UR:
+                b['dir'] == UL
+        ggame.draw.rect(windowSurface, b['color'], b['rect'])
 
